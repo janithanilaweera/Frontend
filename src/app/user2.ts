778dbda66e43f8265ae4e0b6ffd37ed1bf189571
Jay <janitha999@gmail.com>
2020-09-03 17:36:22 +1000

    done more work

---
+++ src/app/user2.ts
@@ -0,0 +1,12 @@
+export class User2 {
+  constructor(
+    public id?: string,
+    public nic?: string,
+    public location?: string,
+    public pricecat?: any,
+    public type?: any ,
+    public price?: any ,
+    public date ?: any) {
+  }
+
+}
